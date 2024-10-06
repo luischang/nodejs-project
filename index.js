d4cf22cd2a7d56b1523acdf3eead0fbba8e868f1
Luis Chang <luis.chang@outlook.com>
2024-10-06 10:37:54 -0500

    Twilio integration

--- index.js
+++ index.js
@@ -7,12 +7,59 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI).then(()=> {
-    console.log("Conectado a MongoDB");
-}).catch((error)=> {
-    console.error("Error al conectar con MongoDB:" , error);
+// mongoose.connect(process.env.MONGODB_URI).then(()=> {
+//     console.log("Conectado a MongoDB");
+// }).catch((error)=> {
+//     console.error("Error al conectar con MongoDB:" , error);
+// });
+
+app.get("/api/sendsms/:number/:message" , (req,res) => {
+    const twilio = require('twilio');
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = new twilio(accountSid, authToken);
+    const number = req.params.number;
+    const messagePhoneNumber = req.params.message;
+
+       client.messages.create({
+            from: '+17174290831',
+            to: '+51' + number,
+            body: messagePhoneNumber
+        }).then(messageResponse => console.log(messageResponse.sid))
+        console.log(messagePhoneNumber);
+        res.json("Ok");
+
 });
+
+app.get("/api/sendwapp/:number/:message" , (req,res) => {
+    const twilio = require('twilio');
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = new twilio(accountSid, authToken);
+    const number = req.params.number;
+    const messagePhoneNumber = req.params.message;
+
+    async function createMessage(){
+        const message = await client.messages.create({
+            from: 'whatsapp:+14155238886',
+            to: 'whatsapp:+51' + number,
+            body: messagePhoneNumber
+        });
+        console.log(messagePhoneNumber);
+    }
+    createMessage();
+});
+
+app.post("/api/webhook",express.json(),(req,res)=>{
+
+    console.log("Recibiendo información: "+ req.body.Body);
+    res.send("send via callback")
+
+})
 
 app.get("/api/items", async (req,res) => {
 
